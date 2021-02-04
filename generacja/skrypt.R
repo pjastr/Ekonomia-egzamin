62be8aaa2791f44a3a629848806afa8d9bdfc547
Piotr <24464854+pjastr@users.noreply.github.com>
2021-02-04 21:55:55 +0100

    e

---
+++ generacja/skrypt.R
@@ -0,0 +1,34 @@
+library(readxl)
+polecenia <- read_excel("polecenia.xlsx", 
+                        col_names = FALSE)
+inne <- read_excel("inne.xlsx", 
+                        col_names = FALSE)
+s0=inne[1,1]
+s1=""
+s2=""
+s3=""
+s4=""
+s5=""
+s6=""
+s7=inne[1,2]
+for (x in 1:50)
+{
+  los=sample(1:24,6)
+  n1=los[1]%%4+1
+  n2=los[2]%%4+1
+  n3=los[3]%%4+1
+  n4=los[4]%%4+1
+  n5=los[5]%%4+1
+  n6=los[6]%%4+1
+  print(paste(n1,n2,n3,n4,n5,n6, sep=","))
+  s1=polecenia[1,n1]
+  s2=polecenia[2,n2]
+  s3=polecenia[3,n3]
+  s4=polecenia[4,n4]
+  s5=polecenia[5,n5]
+  s6=polecenia[6,n6]
+  nazwa=paste("ZestawA",x,".tex", sep="")
+  all=paste(s0,s1,s2,s3,s4,s5,s6,s7,sep="")
+  writeLines(all, nazwa,useBytes=T)
+  tools::texi2pdf(nazwa, clean=T)
+}
